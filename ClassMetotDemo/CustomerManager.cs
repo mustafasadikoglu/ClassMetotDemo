54bdc6a966f00d5ed56897174d121a4b5feaa3a2
Mustafa Sadıkoğlu <m.sadikoglu@msn.com>
2021-01-19 21:48:16 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        
+                         
+        public void Add(Customer customer)
+        {
+            Console.WriteLine($"{customer.FirstName+customer.LastName} isimli müşteri eklendi");
+        }
+       
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine($"{customer.FirstName + customer.LastName} isimli müşteri silindi");
+        }
+        
+        public void List(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName + " " + customer.LastName);
+            }
+        }
+
+       
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer[] c = new Customer[] {
+            new Customer(){CustomerId=1,FirstName="Mustafa",LastName="Sadıkoğlu",TCNo="111111111",YearOfBirth="1993"},
+            new Customer(){CustomerId=2,FirstName="Serhat",LastName="Kapucu",TCNo="222222222",YearOfBirth="1992"} };
+
+            CustomerManager cm = new CustomerManager();
+            cm.Add(c[0]);
+            cm.Add(c[1]);
+            Console.WriteLine("-------------------");
+
+            cm.Delete(c[0]);
+
+            Console.WriteLine("-------------------");
+
+            cm.List(c);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
